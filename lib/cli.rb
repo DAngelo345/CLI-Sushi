a337baddbf410007f29f1518611854b3d469c55a
DAngelo Thomas <dangelo1thomas@gmail.com>
2020-12-11 16:20:21 -0500

    Done.

--- lib/cli.rb
+++ lib/cli.rb
@@ -15,25 +15,43 @@ class CLI
 
     def menu
         puts ""
-        input = gets.strip
+        input = gets.strip.downcase
             # "City #{address}, State #{address}
         API.new("https://api.yelp.com/v3/businesses/search?term=sushi&location=#{input}").get_restaurants
         list_of_restaurants
         puts "___________________"
-        puts""
         # puts "Type in the number of that restaurant to find out the phone number, location and rating of that restaurant."
         puts "Would you like to see the more info about a particular restaurant in the list?"
         puts ""
         puts "Enter yes or no"
         puts ""
-        input = gets.strip.downcase
-        if input == "yes"
-            puts "___________________"
-            puts ""
-            phone_number
-        elsif input == "no"
-            puts "Thanks for using Sushi Time"
+        prompt = "> "
+        print prompt
+        while user_input = gets.strip.downcase
+            case user_input
+            when "yes"
+                phone_number
+            when "no" 
+                puts "Thanks for using Sushi Time"
+                break
+            else
+                puts "Please enter yes or no" 
+                print prompt
+            end
         end
+        
+        # input = gets.strip.downcase
+        # if input == "yes"
+        #     puts "___________________"
+        #     puts ""
+        #     phone_number
+        # elsif input == "no"
+        #     puts "Thanks for using Sushi Time"
+        # else
+        #     return "Wrong input try again"
+        #     input = gets.strip.downcase
+        # end
+       
     end
     
     def list_of_restaurants
@@ -51,14 +69,14 @@ class CLI
         end
     end
 
-    def restaurants_selector
-        puts "Enter the number next to the restaurant you'd like to know more about"
-        input = get.strip.downcase
-        num_input = input.to_i
-        # Restaurant.all.select do |number|
-        #     number.artist == self
-        end
-    end
+    # def restaurants_selector
+    #     puts "Enter the number next to the restaurant you'd like to know more about"
+    #     input = get.strip.downcase
+    #     num_input = input.to_i
+    #     # Restaurant.all.select do |number|
+    #     #     number.artist == self
+    #     end
+    # end
 
     def phone_number
         restaurants = Restaurant.all
